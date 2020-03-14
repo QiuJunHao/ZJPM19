d8155d6426b4c9ecef035b7fb4ac91a359a6dfcd
superpapalei <61541218+superpapalei@users.noreply.github.com>
2020-03-15 05:02:51 +0800

    技能信息测试1

--- src/components/hr/skill.vue
+++ src/components/hr/skill.vue
@@ -1,15 +1,68 @@
 <template>
-  <div class="skill">
-    <div class="containAll">
-      <div class="topLayout">
-        <div class="tbar">
-          <el-button
-           icon="el-icon-refresh" 
-           title="刷新" size="mini" 
-           circle @click="search"
-           ></el-button>
+  <div>
+    <div class="skill">     
+      <div class="tbar">
+        <el-button
+          icon="el-icon-refresh"
+          title="刷新"
+          size="mini"
+          circle
+          @click="search"
+        ></el-button>
+        <el-input
+          @keyup.enter.native="refreshData" v-model="condition"
+          placeholder="请输入内容"
+          style="width:300px;"
+        >
+          <el-button @click="refreshData" slot="append" icon="el-icon-search"
+            >搜索</el-button
+          >
+        </el-input>
 
-     <el-input 
+        <el-button
+          type="primary"
+          style="margin-left:10px;"
+          @click="addNewskill" size="small"
+          >新增
+          </el-button>
+        <el-button type="primary" size="small">导入</el-button>
+     </div>
+     
+      <div class="gridTable">
+          <el-table ref="taskTable" style="width: 100%;" :height="bottomDivShow?'500px':'560px'" :data="taskData"
+            tooltip-effect="dark" highlight-current-row row-key="st_id" default-expand-all
+           >
+            
+            <el-table-column prop="skill_name" label="技能名称" align="center" width="180">
+            </el-table-column>
+            <el-table-column prop="skill_note" label="技能说明" align="center" width="180">
+              <template slot-scope="scope">{{scope.row.dept_id | renderFilter(deptDataFilter)}}
+              </template>
+               <template slot-scope="scope">{{scope.row.st_type | stTypeFilter}}</template>
+            </el-table-column>
+            <el-table-column label="操作" width="140" prop="handle">
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini" circle @click="editTaskShow(scope.row)">
+                </el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini" circle @click="deleteOne(scope.row)">
+                </el-button>
+              </template>
+            </el-table-column>
+            </el-table>
+          </div>
+      </div>
+  </div>
+
+
+              
+ 
+
+
+
+
+
+
+    <!-- <el-input 
           size="small" 
           @keyup.enter.native="refreshData" 
           placeholder="请输入技能名称" 
